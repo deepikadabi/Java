b2fd166195a544dd7f3e1b365f3ea2c03269cc2c
deepikadabi <dabideepika1204@gmail.com>
2020-09-17 12:32:46 +0530

    Scanner Example

---
+++ Day2/InputDemo1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class InputDemo1{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name : ");
+        String i = sc.next();
+        System.out.println("Welcome "+i);
+    }
+}
