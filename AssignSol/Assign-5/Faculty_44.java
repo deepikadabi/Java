311a3b1c798944226bb5c4ead227313f8aa1bff5
deepikadabi <dabideepika1204@gmail.com>
2020-09-22 15:32:26 +0530

    day 23

---
+++ AssignSol/Assign-5/Faculty_44.java
@@ -0,0 +1,9 @@
+//	Create three classes
+//-	Faculty with two data members facultyId and salary and two methods, one intput() method for accepting facultyId as input and another printSalary() to print salary.
+//-	FullTimeFaculty that inherits class Faculty with two data members’ basicSalary and allowance. Override input() method in this class that calls super class inut() method and accepts basicSalary and allowance as input. Salary should not be accepted as input but should be calculated using formula (basicSalary + allowance)
+//-	PartTimeFaculty that inherits class Faculty with two data members’ workingHours, ratePerHour. Override input() method in this class that calls super class inut() method and accepts workingHours and ratePerHour as input. Salary should not be accepted as input but should be calculated using formula ( workingHour * ratePerHour )
+
+
+public class Faculty_44 {
+    
+}
